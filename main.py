3c4aa7cc54b438b4c8349fa6ab4b2133605f550c
Ege Erdağ <egeerdag@gmail.com>
2024-07-25 18:59:58 +0300

    - add function to get the roster. - bug fixes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import praw
 import json
-from nba_api.stats.endpoints import commonplayerinfo
-from nba_api.stats.static import players
+import pandas as pd
+from nba_api.stats.endpoints import commonplayerinfo, commonteamroster
+from nba_api.stats.static import players, teams
 
 # Load the configuration from the JSON file
 with open('config.json') as config_file:
@@ -16,25 +17,43 @@ reddit_instance = praw.Reddit(
     user_agent=config['user_agent']
 )
 
+# getting all the NBA teams
+nba_teams = teams.get_teams()
+
+
+# Getting the roster of a specific team
+def get_team_roster(team_id):
+    roster = commonteamroster.CommonTeamRoster(team_id=team_id)
+    return roster.get_data_frames()[0]
+
+
+# Getting team id for the Golden State Warriors
+warriors_id = [team['id'] for team in nba_teams if team['full_name'] == 'Golden State Warriors'][0]
+
+# Fetch the roster for Golden State Warriors
+warriors_roster = get_team_roster(warriors_id)
+
+# Get the players name
+warriors_players = warriors_roster['PLAYER'].tolist()
+
+
 # Function to get player stats using nba_api
 def get_player_stats(player_name):
     nba_players = players.get_players()
-    player = next((player for player in nba_players if player['full_name'] == player_name), None)
-    if player:
-        player_id = player['id']
+    selected_player = next((selected_player for selected_player in nba_players if selected_player['full_name'] == player_name), None)
+    if selected_player:
+        player_id = selected_player['id']
         player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id)
-        stats = player_info.player_headline_stats.get_dict()
-        if stats['data']:
-            stats = stats['data'][0]  # Get the first (and typically only) entry
+        selected_stats = player_info.player_headline_stats.get_dict()
+        if selected_stats['data']:
+            selected_stats = selected_stats['data'][0]  # Get the first entry
             return {
-                "PPG": stats[3],
-                "RPG": stats[4],
-                "APG": stats[5]
+                "PPG": selected_stats[3],
+                "RPG": selected_stats[4],
+                "APG": selected_stats[5]
             }
     return "Stats not available."
 
-
-players_to_check = ["Stephen Curry", "Draymond Green", "Andrew Wiggins"]
 
 # Get the top 25 posts in the last week
 subreddit = reddit_instance.subreddit("warriors")
@@ -45,7 +64,7 @@ for submission in top_25_submissions:
     comments = submission.comments.list()
 
     for comment in comments:
-        for player in players_to_check:
+        for player in warriors_players:
             if player in comment.body:
                 stats = get_player_stats(player)
                 if stats != "Stats not available.":
@@ -53,7 +72,9 @@ for submission in top_25_submissions:
                 else:
                     reply_text = f"Stats for {player} are not available."
                 comment.reply(reply_text)
-                print(f"I'm a bot and I just replied to comment {comment.id} with {player} stats.")
+                print(f"Just replied to comment {comment.id} with {player} stats.")
+
+print(warriors_players)
 
 # print(reddit_instance.user.me())
 
