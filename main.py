a89fd04ff56f6a364df28fb72e57dd663e212f25
Ege Erdağ <51919614+ErdagEge@users.noreply.github.com>
2025-05-23 11:48:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,33 @@ import logging
 import os
 from config import load_config
 from nba_utils import get_team_roster, get_player_stats
-from reddit_bot import initialize_reddit, get_replied_comment_ids, save_replied_comment_ids, reply_to_comments
+from reddit_bot import (
+    initialize_reddit, get_replied_comment_ids,
+    save_replied_comment_ids, reply_to_comments
+)
 from nba_api.stats.static import teams
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler("bot.log"),
-        logging.StreamHandler()
-    ]
-)
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler("bot.log"),
+            logging.StreamHandler()
+        ]
+    )
 
 def main():
+    setup_logging()
     config_path = os.getenv('DUBNATIONBOT_CONFIG', 'config.json')
     config = load_config(config_path)
     if not config:
+        logging.error("No config loaded. Exiting.")
         return
 
     reddit_instance = initialize_reddit(config)
     if not reddit_instance:
+        logging.error("Reddit initialization failed. Exiting.")
         return
 
     nba_teams = teams.get_teams()
@@ -32,6 +39,7 @@ def main():
 
     warriors_roster = get_team_roster(warriors_id)
     if warriors_roster is None:
+        logging.error("Couldn't fetch Warriors roster.")
         return
 
     warriors_players = warriors_roster['PLAYER'].tolist()
@@ -39,7 +47,13 @@ def main():
     subreddit = reddit_instance.subreddit("warriors")
     replied_comment_ids = get_replied_comment_ids()
 
-    reply_to_comments(subreddit, warriors_players, get_player_stats, replied_comment_ids, stats_cache)
+    reply_to_comments(
+        subreddit,
+        warriors_players,
+        get_player_stats,
+        replied_comment_ids,
+        stats_cache
+    )
 
     save_replied_comment_ids(replied_comment_ids)
 
